4c1036fa119338cebde8f2522dd1a244d10aac36
shyama95 <shyamap95@gmail.com>
2019-11-02 18:20:45 +0530

    Added model file - epochs = 100, time steps = 3

--- test.py
+++ test.py
@@ -5,7 +5,10 @@ import numpy as np
 
 rbm_object = RBM(input_size=28*28, hidden_layer_size=100, time_steps=3)
 data_test = readData('data/test.gz', isTrain=False)
-rbm_object.load_weights()
+rbm_object.load_weights(filename='model_temp.npy')
 
 rbm_object.inference(v=data_test[15, :])
+rbm_object.inference(v=data_test[19, :])
+rbm_object.inference(v=data_test[100, :])
+rbm_object.inference(v=data_test[1500, :])
 
--- train.py
+++ train.py
@@ -6,8 +6,9 @@ import numpy as np
 rbm_object = RBM(input_size=28*28, hidden_layer_size=100, time_steps=3)
 
 data_train = readData('data/train.gz', isTrain=True)
+data_test = readData('data/test.gz', isTrain=False)
 
-rbm_object.train(data=data_train[0:60000, :], learning_rate=0.003, epochs=50)
-rbm_object.save_weights()
+rbm_object.train(data=data_train[0:60000, :], learning_rate=0.003, epochs=100)
+rbm_object.save_weights(filename='model_temp')
 
 rbm_object.inference(v=data_test[15, :])
